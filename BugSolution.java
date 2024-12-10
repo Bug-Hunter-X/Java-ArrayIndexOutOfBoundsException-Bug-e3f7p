6c454cb42e551ff81e230f9650dbafe53683cf02
BugHunter-X <askzenai@gmail.com>
2024-12-10 09:20:05 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int index = 5; // Example of a variable index
+
+        if (index >= 0 && index < arr.length) {
+            arr[index] = 10; 
+            System.out.println(arr[index]);
+        } else {
+            System.out.println("Index out of bounds!");
+        }
+    }
+}
